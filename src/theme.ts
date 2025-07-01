5b3f87b1c4a5262306882ace0482b4a10d5fb9e9
renass21 <renatodossantoss21@gmail.com>
2025-07-01 12:34:41 -0300

    fix: add fonts for components, firstname, lastname and buttons

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,9 @@
 import { createTheme, responsiveFontSizes } from "@mui/material";
 
+import '@fontsource/bungee-outline';
+import '@fontsource/bungee-shade'
+import '@fontsource/monofett';
+
 let theme = createTheme({
   palette: {    
     primary: {
@@ -8,6 +12,28 @@ let theme = createTheme({
     secondary: {
       main: "#4f8e3e",
     },
+  },
+  typography: {
+    
+    fontFamily: [
+      '"Bungee Shade"',
+      '"Bungee Outline"',
+      'cursive',
+      'Roboto',
+      'sans-serif'
+    ].join(','),
+
+    h2: {
+      fontFamily: '"Bungee Shade", cursive',
+      fontSize: '3.2rem',
+      fontWeight: 400,
+    },
+    // Se quisesse que outra variante usasse Bungee Outline por padrão:
+    h3: {
+      fontFamily: '"Monofett", cursive',
+      fontSize: '2.7rem',
+      fontWeight: 300,
+     },
   },
 });
 
