86af7064ceeceba9d37e73aa7108d8cef19a10f6
renass21 <renatodossantoss21@gmail.com>
2025-07-02 21:09:07 -0300

    feat: add Skills and Projects sections with respective components

---
+++ src/pages/sections/projects/Projects.tsx
@@ -0,0 +1,38 @@
+import { styled } from "@mui/material"
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
+import CardProjects from "../../../components/cardProjects/CardProjects"
+
+
+function Projects() {
+
+    const StyledSkills = styled("div")(({ theme }) => ({
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+        height: "100vh",
+        display: "flex",
+        gap: "2rem",
+        alignItems: "center",
+        [theme.breakpoints.up('xs')]: {
+          paddingTop: "50px",
+      },
+      [theme.breakpoints.up('md')]: {
+          paddingTop: "0",
+      },
+  }))
+    return(
+        <StyledSkills>
+            <Container maxWidth="lg">
+            <Box display="flex" alignItems="center" textAlign="center" justifyContent="center" flexDirection="column">
+                <Typography variant="h2" marginBottom="2rem" >
+                    PROJECTS
+                </Typography>
+                <CardProjects />
+            </Box>
+            </Container>
+        </StyledSkills>      
+    )
+}
+
+export default Projects
