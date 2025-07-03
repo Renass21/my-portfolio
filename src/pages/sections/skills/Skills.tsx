b5a06b39269a738a2a103a3ed8bdb85788e3b2b1
renass21 <renatodossantoss21@gmail.com>
2025-07-02 21:09:21 -0300

    feat: add CardSkills component and integrate it into Skills section

---
+++ src/pages/sections/skills/Skills.tsx
@@ -0,0 +1,52 @@
+import { Grid, styled } from "@mui/material"
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
+import CardSkills from "../../../components/cardSkills/CardSkills"
+
+
+function Skills() {
+
+    const StyledSkills = styled("div")(({ theme }) => ({
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+        height: "100vh",
+        display: "flex",
+        gap: "2rem",
+        alignItems: "center",
+        [theme.breakpoints.up('xs')]: {
+          paddingTop: "50px",
+      },
+      [theme.breakpoints.up('md')]: {
+          paddingTop: "0",
+      },
+  }))
+    return(
+        <StyledSkills>
+            <Container maxWidth="lg">
+            <Box display="flex" alignItems="center" textAlign="center" justifyContent="center" flexDirection="column">
+                <Typography variant="h2" marginBottom="2rem" >
+                    SKILLS
+                </Typography>
+               <Grid item xs={12} md={4} gap="1rem" display="flex" justifyContent="center">
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+                <CardSkills />
+               </Grid>
+               
+            </Box>
+            </Container>
+        </StyledSkills>      
+    )
+}
+
+export default Skills
