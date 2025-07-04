c056c429766832dcfadf4083e30aa6426b26f6f7
renass21 <renatodossantoss21@gmail.com>
2025-07-04 12:46:25 -0300

    feat: add interface and data objects skills

---
+++ src/database/skills.ts
@@ -0,0 +1,72 @@
+
+
+export interface Skills {
+    id: number
+    skillImage: string
+    skillName: string
+}
+
+
+export const skills: Skills[] = [
+{
+    id: 1,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+    skillName: "Css3"
+},
+{
+    id: 2,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    skillName: "Javascript"
+},
+{
+    id: 3,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    skillName: "Typescript"
+},
+{
+    id: 4,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg",
+    skillName: "React"
+},
+{
+    id: 5,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg",
+    skillName: "Material UI"
+},
+{   
+    id: 6,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+    skillName: "Next Js"
+},
+{
+    id: 7,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    skillName: "Node"
+},
+{
+    id: 8,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original-wordmark.svg",
+    skillName: "Google Cloud"
+},
+
+{   
+    id: 9,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+    skillName: "Postgresql"
+},
+{   
+    id: 10,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/prisma/prisma-original.svg",
+    skillName: "PrismaORM"
+},
+{   
+    id: 11,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    skillName: "Git"
+},
+{   
+    id: 12,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/axios/axios-plain.svg",
+    skillName: "Axios"
+},
+]
