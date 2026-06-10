eda5d7d0f6e938e49529adf1c7e3aea2bd067768
renass21 <renatodossantoss21@gmail.com>
2026-06-10 15:54:32 -0300

    feat: adicionar novas habilidades com imagens de Jira e TestRail

--- src/database/skills.ts
+++ src/database/skills.ts
@@ -20,62 +20,67 @@ export const skills: Skills[] = [
 },
 {
     id: 3,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jira/jira-original.svg",
+    skillName: "Jira"
+},
+{   
+    id: 4,
+    skillImage: "/assets/images/testrail.svg",
+    skillName: "TestRail"
+},
+{
+    id: 5,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg",
     skillName: "React"
 },
 {
-    id: 4,
+    id: 6,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
     skillName: "Javascript"
 },
 {
-    id: 5,
+    id: 7,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
     skillName: "Typescript"
 },
 {   
-    id: 6,
+    id: 8,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg",
     skillName: "Postman"
 },
 {
-    id: 7,
+    id: 9,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
     skillName: "Css3"
 },
 {
-    id: 8,
+    id: 10,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg",
     skillName: "Material UI"
 },
-{   
-    id: 9,
-    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
-    skillName: "Next Js"
-},
 {
-    id: 10,
+    id: 11,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
     skillName: "Node"
 },
 {
-    id: 11,
+    id: 12,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original-wordmark.svg",
     skillName: "Google Cloud"
 },
 
 {   
-    id: 12,
+    id: 13,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
     skillName: "Postgresql"
 },
 {   
-    id: 13,
+    id: 14,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
     skillName: "Git"
 },
 {   
-    id: 14,
+    id: 15,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/axios/axios-plain.svg",
     skillName: "Axios"
 }
