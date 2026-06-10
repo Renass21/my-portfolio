db87eb0b75984d3df81c895f1ca7750743609922
renass21 <renatodossantoss21@gmail.com>
2026-06-10 14:48:43 -0300

    feat: atualizar lista de habilidades com novas tecnologias e corrigir IDs

--- src/database/skills.ts
+++ src/database/skills.ts
@@ -8,65 +8,75 @@ export interface Skills {
 
 
 export const skills: Skills[] = [
-{
+{   
     id: 1,
-    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
-    skillName: "Css3"
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cypressio/cypressio-original.svg",
+    skillName: "Cypress"
 },
-{
+{   
     id: 2,
-    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-    skillName: "Javascript"
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/playwright/playwright-original.svg",
+    skillName: "Playwright"
 },
 {
     id: 3,
-    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-    skillName: "Typescript"
-},
-{
-    id: 4,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg",
     skillName: "React"
 },
 {
+    id: 4,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    skillName: "Javascript"
+},
+{
     id: 5,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    skillName: "Typescript"
+},
+{   
+    id: 6,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg",
+    skillName: "Postman"
+},
+{
+    id: 7,
+    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+    skillName: "Css3"
+},
+{
+    id: 8,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg",
     skillName: "Material UI"
 },
 {   
-    id: 6,
+    id: 9,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
     skillName: "Next Js"
 },
 {
-    id: 7,
+    id: 10,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
     skillName: "Node"
 },
 {
-    id: 8,
+    id: 11,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original-wordmark.svg",
     skillName: "Google Cloud"
 },
 
 {   
-    id: 9,
+    id: 12,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
     skillName: "Postgresql"
 },
 {   
-    id: 10,
-    skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/prisma/prisma-original.svg",
-    skillName: "PrismaORM"
-},
-{   
-    id: 11,
+    id: 13,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
     skillName: "Git"
 },
 {   
-    id: 12,
+    id: 14,
     skillImage: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/axios/axios-plain.svg",
     skillName: "Axios"
-},
+}
 ]
