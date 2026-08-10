b9e15e159565a937aa40eb94d89a2038e1d46e62
renass21 <renatodossantoss21@gmail.com>
2026-08-09 22:32:52 -0300

    refactor: ajustar formatação e estilo no componente CardSkills

--- src/components/cardSkills/CardSkills.tsx
+++ src/components/cardSkills/CardSkills.tsx
@@ -1,4 +1,4 @@
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 
 interface CardSkillsProps {
   skillName: string;
@@ -9,25 +9,38 @@ function CardSkills({ skillName, skillImage }: CardSkillsProps) {
   return (
     <Box
       sx={{
-        width: '100%',
-        aspectRatio: '1',
+        width: "100%",
+        aspectRatio: "1",
         minWidth: 100,
         borderRadius: 1,
-        bgcolor: '#007FFF',
-        '&:hover': {
-          bgcolor: '#0066CC',
+        bgcolor: "#3e4f52b7",
+        "&:hover": {
+          bgcolor: "#2e4a66",
         },
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
         p: 1,
       }}
     >
-      <img src={skillImage} alt={skillName} style={{ width: 40, height: 40, marginBottom: 8 }} />
-      <span style={{ color: '#fff', fontWeight: 500, fontSize: 14, textAlign: 'center' }}>{skillName}</span>
+      <img
+        src={skillImage}
+        alt={skillName}
+        style={{ width: 40, height: 40, marginBottom: 8 }}
+      />
+      <span
+        style={{
+          color: "#fff",
+          fontWeight: 500,
+          fontSize: 14,
+          textAlign: "center",
+        }}
+      >
+        {skillName}
+      </span>
     </Box>
   );
 }
 
-export default CardSkills
+export default CardSkills;
